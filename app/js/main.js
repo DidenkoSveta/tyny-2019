110141fded8d7482c2280fa8dad6d3c9be83d186
DidenkoSveta <garpia.lana@gmail.com>
2023-12-05 21:08:33 +0400

    checking for errors
    code optimization, adding comments, checking for errors

--- app/js/main.js
+++ app/js/main.js
@@ -82,17 +82,24 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
  //burger menu
  let menuOpenButton = document.querySelector('.header__burger');
  let menu = document.querySelector('.header__menu');
- let menuCloseButton = document.querySelector('.header__close');
  
+ // Обработчик клика для открытия меню
  menuOpenButton.addEventListener('click', () => {
    menu.classList.add('show');
  });
  
+ // Обработчик клика на документе для закрытия меню
  document.addEventListener('click', (e) => {
-   if (e.target.classList.contains('header__close')
-       || !e.target.classList.contains('header__menu')
-       && !e.target.classList.contains('header__burger')) {
+   // Проверяем, кликнул ли пользователь на кнопку закрытия или вне меню
+   if (e.target === menuOpenButton) {
+     // Не делать ничего, если кликнули на кнопку открытия
+     return;
+   }
+ 
+   if (e.target.closest('.header__close') || !e.target.closest('.header__menu')) {
+     // Закрыть меню, если кликнули на кнопку закрытия или вне меню
      menu.classList.remove('show');
    }
  });
+ 
  
